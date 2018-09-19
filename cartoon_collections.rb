aadefabf42aaf193280ac4054f0bfcbbf91a2f1a
Travis Desir <travisgdesir31@gmail.com>
2018-09-19 02:06:16 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,7 @@ def long_planeteer_calls(words)
   words.any? { |word| word.length > 4 }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(cheese)
+  cheese.find { |
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
