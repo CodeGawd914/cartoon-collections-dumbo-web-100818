92b10467a3381eedc54f397d723c0bd88c17b495
Travis Desir <travisgdesir31@gmail.com>
2018-09-19 01:56:43 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def roll_call_dwarves(names)
 end
 
 def summon_captain_planet(planeteer)
-  planeteer.each { |code| code.capitalize
+  planeteer.each { |code| code.capitalize}
 end
 
 def long_planeteer_calls# code an argument here
